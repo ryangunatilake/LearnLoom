3c235e2dd776d314566b3dfeaea9cc27ea7275d8
Dinal Vidushika <96554761+dinalvidushika@users.noreply.github.com>
2024-02-24 11:00:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup/>
+     
+    </div>
+  );
+}
+
+export default App;
