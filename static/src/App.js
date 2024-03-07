b9cbe8ea95ed3115ca7df935e60fe344db7e6aa3
ryangunatilake <116454646+ryangunatilake@users.noreply.github.com>
2024-03-07 12:18:05 +0530

    Update App.js
    routed some new pages.

--- static/src/App.js
+++ static/src/App.js
@@ -7,6 +7,10 @@ import AboutUs from "./Pages/AboutUs";
 import ContactUs from "./Pages/ContactUs";
 import Dashboard from "./Pages/Dashboard";
 import LectureNote from "./Pages/LectureNote";
+import LectureNotePreview from "./Pages/LectureNotePreview";
+import FlashCard   from "./Pages/FlashCard";
+import FlashCardPreview from "./Pages/FlashCardPreview";
+import Quiz from"./Pages/Quiz";
 import Upload from "./Pages/Upload";
 import UploadDone from "./Pages/UploadDone";
 
@@ -19,7 +23,11 @@ function App() {
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/contact-us" element={<ContactUs />} />
           <Route path="/dashboard" element={<Dashboard/>} />
-          <Route path="/lecture-note" element={<LectureNote/>} />
+          <Route path="/lecture-note" element={<LectureNote />} />
+          <Route path="/lecture-note-preview" element={<LectureNotePreview />} /> 
+          <Route path="/flash-card-preview" element={<FlashCardPreview />} />
+          <Route path="/flash-card" element={<FlashCard />} />
+          <Route path="/quiz" element={<Quiz />} />
           <Route path="/upload" element={<Upload/>} />
           <Route path="/upload-done" element={<UploadDone/>} />
           <Route path="*" element={<NotFound />} />
