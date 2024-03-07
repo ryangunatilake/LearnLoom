6b9a3611004862cbb9d3e6581c2f50cb1e086999
ryangunatilake <116454646+ryangunatilake@users.noreply.github.com>
2024-03-07 09:44:58 +0530

    Update App.js
    this is the new app.js with all the relevant new pages added.

--- static/App.js
+++ static/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import AboutUs from "./Pages/AboutUs";
+import ContactUs from "./Pages/ContactUs";
+import Dashboard from "./Pages/Dashboard";
+import LectureNote from "./Pages/LectureNote";
+import Upload from "./Pages/Upload";
+import UploadDone from "./Pages/UploadDone";
 
 function App() {
   return (
-    <div>
-      <LoginSignup/>
-     
+    <div className="App">
+      <Router basename="/Learnloom">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="/lecture-note" element={<LectureNote/>} />
+          <Route path="/upload" element={<Upload/>} />
+          <Route path="/upload-done" element={<UploadDone/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
